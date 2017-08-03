3dd5b9d334cb253a1b1a805edcdfa9dc5a268bc3
Wgaham <jszzhaihao@live.com>
2017-08-03 18:29:26 +0800

    Initial commit

---
+++ app/src/main/java/com/wgaham/uicustomviews/TitleLayout.java
@@ -0,0 +1,35 @@
+package com.wgaham.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * Created by zh on 2017/7/28.
+ */
+
+public class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        LayoutInflater.from(context).inflate(R.layout.title, this);
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "This is your edited text.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
